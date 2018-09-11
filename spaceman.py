f523c5e2b0baabe09007e1216733825664fa5290
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-09-10 20:18:23 -0700

    fixes still in progress

--- spaceman.py
+++ spaceman.py
@@ -1,25 +1,26 @@
 # Requirements:
 #
-#
 
-from collections import Counter
+
+# from collections import Counter
 import random
 
 word_list = ["neptune", "pistachios", "orangutan"]
 
 # secret_word = random.choice(word_list)
 secret_word = "apple"
+secret_list = list(secret_word)
 
 # creates blank phrase to be replaced with correct guesses
-def word_length():
-    masked_word = "-" * len(secret_word)
-    return masked_word
+def masked_word():
+    hidden_word = "-" * len(secret_list)
+    return hidden_word
 
 # counts the number of each letter in the secret word
-def count_letters():
-    counted = {}
-    counted = Counter(secret_word)
-    return counted
+# def count_letters():
+#     counted = {}
+#     counted = Counter(secret_word)
+#     return counted
 
 # gets user input
 def user_input(prompt):
@@ -27,38 +28,41 @@ def user_input(prompt):
     return user_input
 
 # gets index of all occurrences of guessed letter
-def check_word(letter, my_copy_of_secret_word):
-    current_letter = my_copy_of_secret_word.rfind(letter)
+def check_word(letter, copy_secret_word):
+    current_letter = copy_secret_word.rfind(letter)
     index_list = []
     while current_letter != -1:
         index_list.append(current_letter)
-        my_copy_of_secret_word = my_copy_of_secret_word[:current_letter]
-        current_letter = my_copy_of_secret_word.rfind(letter)
+        copy_secret_word = copy_secret_word[:current_letter]
+        current_letter = copy_secret_word.rfind(letter)
     return index_list
 
 # replaces all occurrences of correctly guessed letter in secret word
-word_length
-def replace(check_word, word_length, letter_choice):
+replaced_word = ""
+def replace(check_word, masked_word, letter_choice):
     for x in check_word:
-        word_length = '%s%s%s'%(word_length[:x], letter_choice, word_length[x+1:])
-    return word_length
+        word_h = '%s%s%s'%(masked_word[:x], letter_choice, masked_word[x+1:])
+    return masked_word
 
 # checks guessed letter and allows player to proceed
 def letter_choice(game_choice):
     if game_choice.isalpha() == True & len(game_choice) == 1:
-        check_word(game_choice, secret_word)
+        check_word(game_choice, secret_list)
         return False
     else:
         print("Please do not enter a number or a punctation mark. Please choose one letter at a time. Try again: ")
         return True
 
 # checks if game has been won or lost
-def game_not_won(letter):
+def game_not_won(letter, word):
     # Game still ongoing
-    if replace.find("-") != -1:
-        letter_choice(letter)
+    if replaced_word.find("-") != -1:
+        unguessed_word = masked_word()
+        user_letter = letter_choice(letter)
+        word_index = check_word(user_letter, word)
+        replace(word_index, unguessed_word, user_letter)
     # Win condition
-    elif replace.find("-") >= 0:
+    elif replaced_word.find("-") >= 0:
         print("Congratulations! You're down to earth!")
     # Loss condition
     else:
@@ -67,13 +71,15 @@ def game_not_won(letter):
 # prompts player to guess a letter
 letter_unchosen = True
 while letter_unchosen:
-    print(word_length())
+    print(masked_word())
     choose_letter = user_input("Please select a letter: ")
-    letter_unchosen = game_not_won(choose_letter)
-
+    word = "word"
+    letter_unchosen = game_not_won(choose_letter, word)
 
 # test function
 def test():
     print(count_letters())
-    print(word_length())
+    print(masked_word())
     print(replace([2, 1], "-----", "p"))
+    print(list(secret_word))
+# test()
